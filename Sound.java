0ff7006f780dd3f695a3e44a48c71d86faffcb40
Yin Zheping <yinzh@lafayette.edu>
2020-06-23 22:48:37 -0400

    Add files via upload
    Solved bugs on OSX

--- Sound.java
+++ Sound.java
@@ -13,45 +13,24 @@ import java.io.*;
  * @author 15002
  */
 public class Sound {
-
-
     public static float SAMPLERATE = 8000f;
-    
-     public Sound(){
-        byte[] buf = new byte[(int) SAMPLERATE * 200 / 1000];
-
-        for (int i = 0; i < buf.length; i++) {
-            double angle = i / (SAMPLERATE / 500) * 2.0 * Math.PI;
-            buf[i] = (byte) (Math.sin(angle) * 127.0 * 0);
-        }
-
-        for (int i = 0; i < SAMPLERATE / 100.0 && i < buf.length / 2; i++) {
-            buf[i] = (byte) (buf[i] * i / (SAMPLERATE / 100.0));
-            buf[buf.length - 1 - i]
-                    = (byte) (buf[buf.length - 1 - i] * i / (SAMPLERATE / 100.0));
-        }
-
-        AudioFormat af = new AudioFormat(SAMPLERATE, 8, 1, true, false);
-        try{
-        SourceDataLine sdl = AudioSystem.getSourceDataLine(af);
-        sdl.open(af);
-        sdl.start();
-        }
-        catch(LineUnavailableException e){}
-
-}
-
+    private static int playCount =0;
     public static void changeFreq(int hz) throws LineUnavailableException {
         byte[] buf = new byte[(int) SAMPLERATE * 100 / 1000];
-        for (int i = 0; i < buf.length; i++) {
+        for (int i = 0; i < buf.length-256; i++) {
             double angle = i / (SAMPLERATE / hz) * 2.0 * Math.PI;
-            buf[i] = (byte) (Math.sin(angle) * 127.0 * 0.8);
+            buf[i] = (byte) (Math.sin(angle) * 127.0 * 1);
+        }
+        
+        for(int i = buf.length-256;i<buf.length;i++){
+            double angle = i / (SAMPLERATE / hz) * 2.0 * Math.PI;
+            buf[i] = (byte) (Math.sin(angle) * 127.0 * (buf.length-i)/256);
         }
 
         for (int i = 0; i < SAMPLERATE / 100.0 && i < buf.length / 2; i++) {
             buf[i] = (byte) (buf[i] * i / (SAMPLERATE / 100.0));
             buf[buf.length - 1 - i]
-                    = (byte) (buf[buf.length - 1 - i] * i / (SAMPLERATE / 100.0));
+            = (byte) (buf[buf.length - 1 - i] * i / (SAMPLERATE / 100.0));
         }
 
         AudioFormat af = new AudioFormat(SAMPLERATE, 16, 1, true, false);
@@ -60,12 +39,13 @@ public class Sound {
         sdl.start();
         sdl.write(buf, 0, buf.length);
         sdl.drain();
-        sdl.close();
+        //sdl.close();
+
     }
 
     public void play(int i) {
         try {
-            changeFreq(100 + i * 5);
+                changeFreq(100+i*5);
         } catch (LineUnavailableException e) {
         }
     }
