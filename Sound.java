af4b03d6283911f425d5a69d62ba71394fd8ee84
Zheping Yin <yinzh@lafayette.edu>
2020-06-23 22:29:19 -0400

    Ultimately fixed the pop noise and the bugs on OSX

--- Sound.java
+++ Sound.java
@@ -16,15 +16,13 @@ public class Sound {
 
 
     public static float SAMPLERATE = 8000f;
+    
+     public Sound(){
+        byte[] buf = new byte[(int) SAMPLERATE * 200 / 1000];
 
-    public static void changeFreq(int hz, int ms) throws LineUnavailableException {
-        byte[] buf = new byte[(int) SAMPLERATE * ms / 1000];
-        if (hz > 1000) {
-            hz = 1000;
-        }
         for (int i = 0; i < buf.length; i++) {
-            double angle = i / (SAMPLERATE / hz) * 2.0 * Math.PI;
-            buf[i] = (byte) (Math.sin(angle) * 138.0 * 1);
+            double angle = i / (SAMPLERATE / 500) * 2.0 * Math.PI;
+            buf[i] = (byte) (Math.sin(angle) * 127.0 * 0);
         }
 
         for (int i = 0; i < SAMPLERATE / 100.0 && i < buf.length / 2; i++) {
@@ -34,17 +32,40 @@ public class Sound {
         }
 
         AudioFormat af = new AudioFormat(SAMPLERATE, 8, 1, true, false);
+        try{
+        SourceDataLine sdl = AudioSystem.getSourceDataLine(af);
+        sdl.open(af);
+        sdl.start();
+        }
+        catch(LineUnavailableException e){}
+
+}
+
+    public static void changeFreq(int hz) throws LineUnavailableException {
+        byte[] buf = new byte[(int) SAMPLERATE * 100 / 1000];
+        for (int i = 0; i < buf.length; i++) {
+            double angle = i / (SAMPLERATE / hz) * 2.0 * Math.PI;
+            buf[i] = (byte) (Math.sin(angle) * 127.0 * 0.8);
+        }
+
+        for (int i = 0; i < SAMPLERATE / 100.0 && i < buf.length / 2; i++) {
+            buf[i] = (byte) (buf[i] * i / (SAMPLERATE / 100.0));
+            buf[buf.length - 1 - i]
+                    = (byte) (buf[buf.length - 1 - i] * i / (SAMPLERATE / 100.0));
+        }
+
+        AudioFormat af = new AudioFormat(SAMPLERATE, 16, 1, true, false);
         SourceDataLine sdl = AudioSystem.getSourceDataLine(af);
         sdl.open(af);
         sdl.start();
         sdl.write(buf, 0, buf.length);
-//        sdl.drain();
-//        sdl.close();
+        sdl.drain();
+        sdl.close();
     }
 
     public void play(int i) {
         try {
-            changeFreq(200 + i * 5, 100);
+            changeFreq(100 + i * 5);
         } catch (LineUnavailableException e) {
         }
     }
